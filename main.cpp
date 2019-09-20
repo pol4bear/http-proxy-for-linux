8734bb4ebc31009bd8b009a102f56e5e60281c38
김현홍 <kr.hyunhong@gmail.com>
2019-09-21 03:27:04 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,46 +3,10 @@
 # include <stdlib.h>
 # include <arpa/inet.h>
 # include "Include/webproxy.h"
+# include "Include/eventmanager.h"
+
 
 using namespace std;
-
-
-/***** Constant *****/
-static const string info = "[i]";
-static const string positive = "[+]";
-static const string negative = "[-]";
-static const string error = "[!]";
-
-
-/***** I/O Method *****/
-static void newMessage(string prefix, string message)
-{
-    cout << prefix << " " << message << "\n";
-}
-
-
-/***** Callback *****/
-static void onNewClient(int client_socket, sockaddr client_address)
-{
-    in_addr client_ip = reinterpret_cast<sockaddr_in*>(&client_address)->sin_addr;
-    newMessage(positive, "New connection from " + string(inet_ntoa(client_ip)) + " started at socket " + to_string(client_socket));
-}
-
-static void onClientDisconnect(int client_socket)
-{
-    newMessage(negative, "Socket " + to_string(client_socket) + " Disconnected from server");
-}
-
-
-static void onNewRequestReceived(int client_socket, string message)
-{
-    newMessage(positive, "New request from socket " + to_string(client_socket) + " " + message);
-}
-
-static void onNewResponseSent(int client_socket, string message)
-{
-    newMessage(positive, "Response for " + message + " sent to socket " + to_string(client_socket) + " successfully");
-}
 
 
 /***** Main *****/
@@ -63,10 +27,10 @@ int main(int argc, char *argv[])
 
     WebProxy web_proxy;
     // Set callback
-    web_proxy.setSendNewClientConnected(onNewClient);
-    web_proxy.setSendClientDisconnected(onClientDisconnect);
-    web_proxy.setSendNewRequestReceived(onNewRequestReceived);
-    web_proxy.setSendNewResponseSent(onNewResponseSent);
+    web_proxy.setSendNewClientConnected(EventManager::onNewClient);
+    web_proxy.setSendClientDisconnected(EventManager::onClientDisconnect);
+    web_proxy.setSendNewRequestReceived(EventManager::onNewRequestReceived);
+    web_proxy.setSendNewResponseSent(EventManager::onNewResponseSent);
 
     newMessage(info, "Web Proxy Initialized");
 
